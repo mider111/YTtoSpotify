72078776f77c839bb1e965cef2a8ae40e76ab885
Viktor Ivanov <vik.ivanov98@gmail.com>
2020-07-26 18:36:43 -0400

    Remove useless text from track titles

--- convert.py
+++ convert.py
@@ -1,12 +1,14 @@
-# TODO 1. Get YT playlist and video titles ----- DONE
-# TODO 2. Log in spotify and create empty playlist
-# TODO 3. Convert playlist
-from pprint import pprint
+# TODO Get YT playlist and video titles ----- DONE
+# TODO Clean titles. ----- DONE
+# TODO Log in spotify and create empty playlist
+# TODO Add songs to playlist
 
 from googleapiclient.discovery import build
 import os
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyOAuth
+import re
 
 youtube_token = os.environ.get('YT')
 
@@ -44,17 +46,39 @@ def get_playlist_videos(yt_api_token, playlist_id):
 
 
 def create_spotify_playlist(username_id):
-    auth_manager = SpotifyClientCredentials()
-    sp = spotipy.Spotify(auth_manager=auth_manager)
+    ''' spotify_token = util.prompt_for_user_token(
+        username=username_id,
+        scope="playlist-modify-public",
+        client_id=os.environ.get('SPOTIPY_CLIENT_ID'),
+        client_secret=os.environ.get('SPOTIPY_CLIENT_SECRET'),
+        redirect_uri=os.environ.get('SPOTIPY_REDIRECT_URI')
+    )'''
+    scope = 'playlist-modify-public'
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    sp.user_playlist_create(username_id, 'YouTube Converter PL', public=True, description='Test')
+
 
 def clean_titles(videos):
-    for v in videos: 
+    clean_list = []
+    ''' IGNORE = ['(Music Video)', '(Official Music Video)', '(Audio)',
+              '(Lyrics)', '[Official Audio]', '(Lyrics)', '(Official Video)',
+              '(Lyric Video)', 'Lyrics', '[Official Music Video]',
+              ] '''
+    regex = '(\[|\().*(\]|\))'
+    for v in videos:
+        result = re.search(regex, v)
+        if result:
+            v = re.sub(regex, '', v)
+            clean_list.append(v)
+    return clean_list
+
 
 def main():
     videos = get_playlist_videos(youtube_token, 'PLxA687tYuMWjuNRTGvDuLQZjHaLQv3wYL')
     print(len(videos))
     print(videos)
     create_spotify_playlist('wwohpbbvy3sz9ul8fspapyp2o')
+    print(clean_titles(videos))
 
 
 if __name__ == "__main__":
